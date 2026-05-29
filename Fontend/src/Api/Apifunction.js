5730d265c4b9ce6426412c9686a181a8ae92d7d8
abdulbahad123 <abdulbahad.dev@gmail.com>
2026-05-29 17:31:06 +0530

    file

--- Fontend/src/Api/Apifunction.js
+++ Fontend/src/Api/Apifunction.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.DEV ? "http://localhost:3000" : "https://chatbot-anco.onrender.com/";
+const API_BASE_URL = import.meta.env.DEV ? "http://localhost:3000" : "https://chatbot-anco.onrender.com";
 
 export const projectapi = async (formData) => {
 
